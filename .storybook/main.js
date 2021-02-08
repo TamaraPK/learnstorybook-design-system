d163fa14365844696aa5b31797c6ab9c2527e581
Tamara Goudian <tamara.goudian@primekey.com>
2021-02-08 18:33:52 +0100

    Added documentation regular, mdx, and custom mdx page, in addition to build config for doc mode

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,17 @@
+// .storybook/main.js
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  // changes the load order of our stories. First loads the Intro page
+  // automatically import all files ending in *.stories.js|mdx
+  stories: [
+    '../src/Intro.stories.mdx',
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
     '@storybook/addon-a11y',
   ],
-}
+};
